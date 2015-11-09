eebaa51cf34ef98a042ada51dfee9354f24557bc
BlackDiscGH <avinashjham@Avinashs-MacBook-Pro.local>
2015-11-09 17:08:29 -0500

    Adding feature to edit pictures

--- config/routes.rb
+++ config/routes.rb
@@ -63,4 +63,7 @@ get 'pictures/new' => 'pictures#new'
 
 get 'pictures/:id' => 'pictures#show', as: 'picture'
 
+get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+patch 'pictures/:id' => "pictures#update"
+
 end
